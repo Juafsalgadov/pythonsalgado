a8b5bd22b0cdf5a57d76f78a6c1e0c0f47d35305
Juan Salgadov <juanfelipesalgadovarelacole@gmail.com>
2023-05-04 22:36:54 -0500

    Tarea

---
+++ Ejercicios_15/Ejercicio_12.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+if num1 > num2:
+    larger_number = num1
+else:
+    larger_number = num2
+print("El número más grande es:", larger_number)
